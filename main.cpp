9ac0e1f62b6fb2d6ab4df32e6b8375f609ab6a85
Ivan Gorsky <lesaltermans@gmail.com>
2021-03-05 18:32:47 +0300

    added Robot class and it's construct with connection to server

--- main.cpp
+++ main.cpp
@@ -1,12 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include "helpers.h"
 
-class Robot()
+class Robot
 {
+    private:
+        int sockfd;
+    public:
+        Robot(char* ip_addr, char *port)
+        {
+            struct sockaddr_in addr;
+            addr.sin_family = AF_INET;
+            addr.sin_port = htons(str_to_int(port));
+            if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+                exit_with_perror(sock_error);
+            if (!inet_aton(ip_addr, &(addr.sin_addr)))
+                exit_with_perror(invalid_ip);
+            if (connect(sockfd, (struct sockaddr *) &addr, sizeof(addr)))
+                exit_with_perror(connect_error);
+        }
+};
 
-}
-
-int main()
+int main(int argc, char *argv[])
 {
+    check_params_num(argc);
+    Robot r(argv[1], argv[2]);
     return 0;
 }
