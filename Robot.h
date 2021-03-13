047b5f80362d6feca095e5e034052b67403491cf
Ivan Gorsky <lesaltermans@gmail.com>
2021-03-13 19:44:19 +0300

    Added lot of Robot.cpp function's headers

--- Robot.h
+++ Robot.h
@@ -4,10 +4,26 @@
 #include "stdio.h"
 #include "helpers.h"
 #include "unistd.h"
+#include "stdlib.h"
+#include "string.h"
+
+struct market
+{
+    int raw;
+    int min_price;
+    int prod;
+    int max_price;
+};
 
 class Robot
 {
 private:
+    market m;
+    int raw;
+    int prod;
+    int money;
+    int plants;
+    int auto_plants;
     int sockfd;
     char buffer[BUFFER_SIZE];
 public:
@@ -27,7 +43,46 @@ public:
         dprintf(sockfd, "%s", msg);
     }
 
+    void print_buffer()
+    {
+        printf("%s\n", buffer);
+    }
+
     void rcv_server_msg();
+
+    void wait_for_start()
+    {
+        while (strncmp("START", buffer + 2, 5) != 0)
+            rcv_server_msg();
+    }
+
+    void get_info()
+    {
+        snd_server_msg("info\n");
+        rcv_server_msg();
+    }
+
+    void get_market()
+    {
+        snd_server_msg("market\n");
+        rcv_server_msg();
+    }
+
+    void parse_market()
+    {
+        int *fields[4] = {&m.raw, &m.min_price, &m.prod, &m.max_price};
+        parse(fields, 4);
+    }
+
+    void parse_info()
+    {
+        int *fields[5] = {&raw, &prod, &money, &plants, &auto_plants};
+        parse(fields, 5);
+    }
+
+    void parse(int *params[], int params_num);
+
+    char *parse_str(int& i);
 
 };
 
