676cc95b7c07e33fc3ad8e34af4222977664fe27
Ivan Gorsky <lesaltermans@gmail.com>
2021-03-13 14:25:42 +0300

    new error strings unum next_action buffer_size const check argv function's headers

--- helpers.h
+++ helpers.h
@@ -1,9 +1,19 @@
 #ifndef ROBOT_MANAGER_HELPERS_H
 #define ROBOT_MANAGER_HELPERS_H
 
+#define BUFFER_SIZE 128
+
+enum next_action {
+    join,
+    create,
+    quit
+};
+
 const char sock_error[] = "Socket problem";
-const char invalid_params[] = "Invalid number of params. Usage ./prog_name "
-                              "ip_addr port\n";
+const char invalid_params_num[] = "Invalid number of params.";
+const char invalid_params[] = "Invalid params.";
+const char usage[] = "Usage ./prog_name <ip_addr> <port> join <room_num> | "
+                     "create \n";
 const char invalid_ip[] = "Invalid ip addr";
 const char connect_error[] = "Error while connect to server";
 
@@ -11,7 +21,13 @@ int str_to_int(char *s);
 
 void exit_with_perror(const char buffer[]);
 
-void check_params_num(int argc);
+int check_params_num(int argc);
+
+int check_params(int argc, char *argv[]);
+
+int check_join(int argc, char *argv[]);
+
+int check_create(int argc, char *argv[]);
 
 
 #endif //ROBOT_MANAGER_HELPERS_H
