ab6779b24b8cb6cfc9fbe05cc23af800a65fe68a
Ivan Gorsky <lesaltermans@gmail.com>
2021-03-13 14:26:21 +0300

    check params functions

--- helpers.cpp
+++ helpers.cpp
@@ -28,11 +28,32 @@ void exit_with_perror(const char buffer[])
     exit(1);
 }
 
-void check_params_num(int argc)
+int check_params_num(int argc)
 {
-    if (argc != 3)
+    return argc < 4 || argc > 5;
+}
+
+int check_params(int argc, char *argv[])
+{
+    if (!check_params_num(argc))
     {
-        printf(invalid_params);
-        exit(1);
+        printf("%s %s\n", invalid_params_num, usage);
+        return quit;
     }
+    else if (check_join(argc, argv))
+        return join;
+    else if (check_create(argc, argv))
+        return create;
+    return quit;
+}
+
+int check_join(int argc, char *argv[])
+{
+    return (argc == 5) && (strcmp(argv[3], "join") == 0)
+    && (strlen(argv[4]) < 12);
+}
+
+int check_create(int argc, char *argv[])
+{
+    return (argc == 4) && (strcmp(argv[3], "create") == 0);
 }
