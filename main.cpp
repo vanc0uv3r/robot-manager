d0b2e453829e4e3e2c2e054758f90df1caea138c
Ivan Gorsky <lesaltermans@gmail.com>
2021-03-16 14:54:14 +0300

    count round and nickname accept

--- main.cpp
+++ main.cpp
@@ -5,10 +5,11 @@
 
 void play(Robot& r)
 {
-    r.wait_for_start();
-    printf("GAME HAS STARTED\n");
+    int round = 1;
+    printf("GAME HAS STARTED\n\n");
     while (r.no_winner())
     {
+        printf("ROUND %d:\n\n", round);
         r.get_info();
         r.parse_info();
         r.get_market();
@@ -20,6 +21,7 @@ void play(Robot& r)
         r.make_turn();
         r.wait_other();
         r.print_buffer();
+        round++;
     }
     r.define_winner();
 }
@@ -28,15 +30,12 @@ int main(int argc, char *argv[])
 {
     int next_action = check_params(argc, argv);
     if (next_action == quit)
-    {
-        printf("%s %s\n", invalid_params, usage);
         return 1;
-    }
-    Robot r(argv[1], argv[2]);
+    Robot r(argv[1], argv[2], argv[3]);
     if (next_action == join)
-        r.join(argv[4]);
+        r.join(argv[5]);
     else if (next_action == create)
-        r.create();
+        r.create(argv[5]);
     play(r);
     return 0;
 }
