f130ebd18b7c5a66ab0bce050b3ef3b2a90c5c5a
Ivan Gorsky <lesaltermans@gmail.com>
2021-03-25 16:10:16 +0300

    lexeme types lexeme and list of lexemes structs Construct in Lex.cpp

--- Lex.cpp
+++ Lex.cpp
@@ -1,2 +1,22 @@
 #include "Lex.h"
 
+Lex::Lex(const char *filename)
+{
+    buffer = new char [128];
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+    {
+        perror("file");
+        exit(1);
+    }
+    dup2(fd, 0);
+    close(fd);
+}
+
+int Lex::next_lexeme() {
+    return 0;
+}
+
+int Lex::define_lexeme_type() {
+    return 0;
+}
--- Lex.h
+++ Lex.h
@@ -1,28 +1,51 @@
 #ifndef ROBOT_MANAGER_LEX_H
 #define ROBOT_MANAGER_LEX_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 
+enum lexeme_types
+{
+    separator,
+    num_const,
+    str_const,
+    variable,
+    label,
+    function,
+    key_word
+};
+
+struct lexeme
+{
+    int line;
+    int type;
+    char *name;
+};
+
+struct list
+{
+    lexeme *l;
+    list *next;
+};
+
 class Lex
 {
     int fd;
+    int c;
+    char *buffer;
+    lexeme l;
+    list lexeme_list;
 
-    Lex(char *filename)
-    {
-        fd = open(filename, O_RDONLY);
-        if (fd == -1)
-        {
-            printf("Error\n");
-            exit(1);
-        }
-    }
+public:
+    Lex(const char *filename);
 
-    ~Lex()
-    {
-        close(fd);
-    }
+    ~Lex(){ delete[] buffer;}
+
+    int define_lexeme_type();
+
+    int next_lexeme();
 };
 
 
