d77e54bd7b4b57b0a3f8317abd2e2a0e450cd2d3
Ivan Gorsky <lesaltermans@gmail.com>
2021-05-03 21:59:00 +0300

    move methods code from .h to .cpp

--- Syntax/ErrorSyntax.cpp
+++ Syntax/ErrorSyntax.cpp
@@ -1,1 +1,49 @@
 #include "ErrorSyntax.h"
+
+
+ErrorSyntax::ErrorSyntax(int lex_line, const char *str, char *lexeme)
+{
+    int str_size = strlen(str);
+    line = lex_line;
+    broken_lexeme = NULL;
+    msg = new char[str_size + 1];
+    strncpy(msg, str, str_size);
+    msg[str_size] = '\0';
+    copy_str(lexeme, str_size);
+}
+
+ErrorSyntax::ErrorSyntax(const ErrorSyntax &err_syn)
+{
+    int str_size = strlen(err_syn.msg);
+    line = err_syn.line;
+    msg = new char[str_size + 1];
+    strncpy(msg, err_syn.msg, str_size);
+    msg[str_size] = '\0';
+    copy_str(err_syn.broken_lexeme, str_size);
+}
+
+ErrorSyntax::~ErrorSyntax()
+{
+    delete [] msg;
+    if (broken_lexeme)
+        delete [] broken_lexeme;
+}
+
+void ErrorSyntax::copy_str(char *lexeme, int str_size)
+{
+    if (lexeme != NULL)
+    {
+        str_size = strlen(lexeme);
+        broken_lexeme = new char[str_size + 1];
+        strncpy(broken_lexeme, lexeme, str_size);
+        broken_lexeme[str_size] = '\0';
+    }
+}
+
+void ErrorSyntax::error_msg() const
+{
+    if (broken_lexeme)
+        printf("%s on line %d. Found %s\n",  msg, line, broken_lexeme);
+    else
+        printf("%s on line %d\n", msg, line);
+}
--- Syntax/ErrorSyntax.h
+++ Syntax/ErrorSyntax.h
@@ -9,54 +9,16 @@ class ErrorSyntax
     char *msg;
     char *broken_lexeme;
 
+    void copy_str(char *lexeme, int str_size);
+
 public:
-    ErrorSyntax(int lex_line, const char *str, char *lexeme)
-    {
-       int str_size = strlen(str);
-       line = lex_line;
-       broken_lexeme = NULL;
-       msg = new char [str_size + 1];
-       strncpy(msg, str, str_size);
-       msg[str_size] = '\0';
-       if (lexeme != NULL)
-       {
-           str_size = strlen(lexeme);
-           broken_lexeme = new char [str_size + 1];
-           strncpy(broken_lexeme, lexeme, str_size);
-           broken_lexeme[str_size] = '\0';
-       }
-    }
+    ErrorSyntax(int lex_line, const char *str, char *lexeme);
 
-    ErrorSyntax(const ErrorSyntax &err_syn)
-    {
-        int str_size = strlen(err_syn.msg);
-        line = err_syn.line;
-        msg = new char [str_size + 1];
-        strncpy(msg, err_syn.msg, str_size);
-        msg[str_size] = '\0';
-        if (err_syn.broken_lexeme != NULL)
-        {
-            str_size = strlen(err_syn.broken_lexeme);
-            broken_lexeme = new char[str_size + 1];
-            strncpy(broken_lexeme, err_syn.broken_lexeme, str_size);
-            broken_lexeme[str_size] = '\0';
-        }
-    }
+    ErrorSyntax(const ErrorSyntax &err_syn);
 
-    ~ErrorSyntax()
-    {
-        delete [] msg;
-        if (broken_lexeme)
-            delete [] broken_lexeme;
-    }
+    ~ErrorSyntax();
 
-    void error_msg() const
-    {
-        if (broken_lexeme)
-            printf("%s on line %d. Found %s\n",  msg, line, broken_lexeme);
-        else
-            printf("%s on line %d\n", msg, line);
-    }
+    void error_msg() const;
 
 };
 
